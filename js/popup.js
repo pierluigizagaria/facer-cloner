e679d87af353b87aece53402461659f22a1d9485
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2022-09-05 16:03:26 +0200

    Added url check

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,15 @@
+const facerWatchfaceRegex = /https:\/\/www\.facer\.io\/watchface\/[a-zA-Z0-9]+\??.*/;
+const domUrlInput = document.querySelector('#url-input');
 
 async function clone() {
+    const url = domUrlInput.value;
+    if (!facerWatchfaceRegex.test(url)) {
+        domUrlInput.value = '';
+        return;
+    }
     const [{ id: tabId }] = await chrome.tabs.query({ active: true, currentWindow: true });
-    const url = document.querySelector('#url-input').value;
     chrome.runtime.sendMessage({ type: 'clone', params: { tabId, url } });
+    window.close();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
